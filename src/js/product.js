98113f869ba9ed4ee56aa1f6c9ba502ac2353148
jdethington <jdethington@gmail.com>
2026-07-27 19:28:04 -0700

    Getting started. Removed unused files. Added needed files

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
 import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import MovieData from "./MovieData.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new MovieData("tents");
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,64 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+// Return parameter from URL
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  // if clear is true we need to clear out the contents of the parent.
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+
+  renderWithTemplate(headerTemplate, headerElement);
+
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
+// Create alert message to display at top of
+export function alertMessage(message, scroll = true, duration = 3000) {
+  // 1. Create alert element
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.classList.add("form-alert");
+  // 2. Set content (Proper </p> tag & 'X' inside <span>)
+  alert.innerHTML = `<p class="alert form-alert">${message}</p><span class="delete-button">X</span>`;
+  // 3. Add close button listener
+  // ====================== USE setClick function from earlier =================================================
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName === "SPAN" || e.target.innerText === "X") {
+      main.removeChild(this);
+    }
+  });
+  // 4. Select <main> and insert alert at the top
+  const main = document.querySelector("main");
+  main.prepend(alert);
+  // 5. Scroll to top if requested
+  if (scroll) {
+    window.scrollTo(0, 0);
+  }
+  // alert removed after duration
+  setTimeout(() => {
+    main.removeChild(alert);
+  }, duration);
+}
